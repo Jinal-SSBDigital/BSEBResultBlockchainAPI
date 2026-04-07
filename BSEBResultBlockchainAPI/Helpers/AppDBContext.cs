1eeaa71ca894c912351540d3f95c19fde8bf9a3c
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-04-07 16:55:06 +0530

    Add Db context

---
+++ BSEBResultBlockchainAPI/Helpers/AppDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BSEBResultBlockchainAPI.Helpers
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+    }
+}
