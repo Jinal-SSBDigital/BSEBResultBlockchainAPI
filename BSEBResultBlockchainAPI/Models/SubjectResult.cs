2c1b87943219dd92d159e5636ebb6be8f15dd378
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-04-07 16:53:33 +0530

    Add Student Model

---
+++ BSEBResultBlockchainAPI/Models/SubjectResult.cs
@@ -0,0 +1,16 @@
+﻿namespace BSEBResultBlockchainAPI.Models
+{
+    public class SubjectResult
+    {
+        public string? Sub { get; set; }
+        public int? MaxMark { get; set; }
+        public int? PassMark { get; set; }
+        public string? Theory { get; set; }
+        public string? OB_PR { get; set; }
+        public string? GRC_THO { get; set; }
+        public string? GRC_PR { get; set; }
+        public string? CCEMarks { get; set; }
+        public string? TotSub { get; set; }
+        public string? SubjectGroupName { get; set; }
+    }
+}
