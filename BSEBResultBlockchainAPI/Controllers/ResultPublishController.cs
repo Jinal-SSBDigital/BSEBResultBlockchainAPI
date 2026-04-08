3300387d0f0b5cf0c8448fd7c40eaa31e0ac92a1
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-04-08 14:25:13 +0530

    changes decrypt code

--- BSEBResultBlockchainAPI/Controllers/ResultPublishController.cs
+++ BSEBResultBlockchainAPI/Controllers/ResultPublishController.cs
@@ -1,4 +1,5 @@
-﻿using BSEBResultBlockchainAPI.Services.Interfaces;
+﻿using BSEBResultBlockchainAPI.Services;
+using BSEBResultBlockchainAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BSEBResultBlockchainAPI.Controllers
@@ -8,10 +9,12 @@ namespace BSEBResultBlockchainAPI.Controllers
     public class ResultPublishController : ControllerBase
     {
         private readonly IResultPublishService _publishService;
+        private readonly IFlureeService _flureeService;
 
-        public ResultPublishController(IResultPublishService publishService)
+        public ResultPublishController(IResultPublishService publishService, IFlureeService flureeService)
         {
             _publishService = publishService;
+            _flureeService= flureeService;
         }
 
        
@@ -21,6 +24,24 @@ namespace BSEBResultBlockchainAPI.Controllers
             // Fire-and-forget — runs in background, API returns immediately
             _ = Task.Run(() => _publishService.PublishAllResultsAsync());
             return Accepted(new { message = "Publishing started in background." });
+        }
+
+        [HttpGet("decrypt")]
+        public async Task<IActionResult> GetDecrypted(string rollCode, string rollNo)
+        {
+            try
+            {
+                var result = await _flureeService.GetDecryptedLatestAsync(rollCode, rollNo);
+
+                if (result == null)
+                    return NotFound(new { message = "Record not found or empty" });
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
         }
     }
 }
