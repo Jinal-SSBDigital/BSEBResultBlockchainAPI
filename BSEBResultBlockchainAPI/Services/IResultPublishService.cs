a14072edc78354965021d5d436f345b3643644ff
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-04-27 15:52:57 +0530

    add new method for batch

--- BSEBResultBlockchainAPI/Services/IResultPublishService.cs
+++ BSEBResultBlockchainAPI/Services/IResultPublishService.cs
@@ -3,6 +3,7 @@
     public interface IResultPublishService
     {
         Task PublishAllResultsAsync(CancellationToken cancellationToken = default);
+        Task PublishAllResultsAsyncNew(CancellationToken cancellationToken = default);
         Task Encv2PublishAllResultsAsync(CancellationToken cancellationToken = default);
     }
 }
